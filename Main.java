6304db92228366705cf339ec6a6734449100d1bc
Jay Solanki <98963173+MyGIT9999@users.noreply.github.com>
2023-05-21 16:16:33 +0530

    negative number test case
    negative number test case

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Calculate.add("//;n7;6;58");
+        Calculate.add("//;n7;-6;-58;98;4;7;-3");
     }
 }
