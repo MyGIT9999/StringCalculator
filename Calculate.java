c85b4e96d5bc953b866a168e08115594c1091f1d
Jay Solanki <98963173+MyGIT9999@users.noreply.github.com>
2023-05-21 16:13:38 +0530

    Negative number testcase
    Negative number testcase

--- Calculate.java
+++ Calculate.java
@@ -6,15 +6,23 @@ import java.util.ArrayList;
 public class Calculate {
 
 
-    public static int add(final String numbers) {
-        String delimiter = ",|n";
-        String numbersWithoutDelimiter = numbers;
-        if (numbers.startsWith("//")) {
-            int delimiterIndex = numbers.indexOf("//") + 2;
-            delimiter = numbers.substring(delimiterIndex, delimiterIndex + 1);
-            numbersWithoutDelimiter = numbers.substring(numbers.indexOf("n") + 1);
+    private static int add(final String numbers, final String delimiter) {
+        int returnValue = 0;
+        String[] numbersArray = numbers.split(delimiter);
+        List negativeNumbers = new ArrayList();
+        for (String number : numbersArray) {
+            if (!number.trim().isEmpty()) {
+                int numberInt = Integer.parseInt(number.trim());
+                if (numberInt < 0) {
+                    negativeNumbers.add(numberInt);
+                }
+                returnValue += numberInt;
+            }
         }
-        return add(numbersWithoutDelimiter, delimiter);
+        if (negativeNumbers.size() > 0) {
+            throw new RuntimeException("Negatives not allowed: " + negativeNumbers.toString());
+        }
+        return returnValue;
     }
 
     private static int add(final String numbers, final String delimiter) {
